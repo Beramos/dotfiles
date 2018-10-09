80794667d94ccb30ac411dff5c5b317472cac5dc
beramos <bram.de.jaegher@gmail.com>
2018-10-09 12:53:39 +0200

    basic program structure

--- note-template/note.sh
+++ note-template/note.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# 1. Fetch date
+# 1.b (OPTIONAL) Change branch?
+# 2. Input attendents
+# 3. Input title prefix
+# 4. Input keywords
+# 5. Generate file 
+# 6. Open with text editor 
